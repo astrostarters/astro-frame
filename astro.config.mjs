dcc6386bbabf078749b994ad6a08c6c1b424d28e
MAZE <remvze@proton.me>
2026-04-22 13:35:13 +0330

    Add basic blog list

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,7 +7,7 @@ import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
   site: "https://eample.com",
-  integrations: [react(), sitemap(), robotsTxt()],
+  integrations: [react(), mdx(), sitemap(), robotsTxt()],
   vite: {
     plugins: [tailwindcss()],
   },
